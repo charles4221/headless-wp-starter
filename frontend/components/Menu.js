4ec334a1795879bc78d590133b6834862e510278
Charles Harwood <charles@guerrilla.com.au>
2018-11-24 12:38:59 +1000

    Customised linting config, added nprogress for loading indicators

--- frontend/components/Menu.js
+++ frontend/components/Menu.js
@@ -1,54 +1,58 @@
-import React, { Component } from "react";
-import Link from "next/link";
-import { Config } from "../config.js";
+import React, { Component } from 'react';
+import Link from 'next/link';
+import { Config } from '../config.js';
 
 const linkStyle = {
-    marginRight: 15
+	marginRight: 15
 };
 
 class Menu extends Component {
-  constructor() {
-      super();
-  }
 
-  getSlug(url) {
-      const parts = url.split("/");
-      return parts.length > 2 ? parts[parts.length - 2] : "";
-  }
+	constructor() {
+		super();
+	}
 
-  render() {
-      const menuItems = this.props.menu.items.map((item, index) => {
-        if (item.object === "custom") {
-            return (
-                <Link prefetch href={item.url} key={item.ID}>
-                    <a style={linkStyle}>{item.title}</a>
-                </Link>
-            );
-        }
-        const slug = this.getSlug(item.url);
-        const actualPage = item.object === "category" ? "category" : "post";
-        return (
-            <Link
-                prefetch
-                as={`/${item.object}/${slug}`}
-                href={`/${actualPage}?slug=${slug}&apiRoute=${item.object}`}
-                key={item.ID}
-            >
-                <a style={linkStyle}>{item.title}</a>
-            </Link>
-        );
-    });
+	getSlug(url) {
+		const parts = url.split('/');
+
+		return parts.length > 2 ? parts[parts.length - 2] : '';
+	}
+
+	render() {
+		const menuItems = this.props.menu.items.map((item, index) => {
+			if (item.object === 'custom') {
+				return (
+					<Link prefetch href={ item.url } key={ item.ID }>
+						<a style={ linkStyle }>{ item.title }</a>
+					</Link>
+				);
+			}
+			const slug = this.getSlug(item.url);
+			const actualPage = item.object === 'category' ? 'category' : 'post';
 
 
-    return(
-      <div>
-          <Link href="/">
-              <a style={linkStyle}>Home</a>
-          </Link>
-          {menuItems}
-      </div>
-    )
-  }
+			return (
+				<Link
+					prefetch
+					as={ `/${slug}` }
+					href={ `/${actualPage}?slug=${slug}&apiRoute=${item.object}` }
+					key={ item.ID }
+				>
+					<a style={ linkStyle }>{ item.title }</a>
+				</Link>
+			);
+		});
+
+
+		return (
+			<div>
+				<Link href="/">
+					<a style={ linkStyle }>Home</a>
+				</Link>
+				{ menuItems }
+			</div>
+		)
+	}
 
 
 }
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,91 +1,103 @@
-import Layout from "../components/Layout.js";
-import React, { Component } from "react";
-import fetch from "isomorphic-unfetch";
-import Link from "next/link";
+import Layout from '../components/Layout.js';
+import React, { Component } from 'react';
+import fetch from 'isomorphic-unfetch';
+import Link from 'next/link';
 import Router from 'next/router';
-import PageWrapper from "../components/PageWrapper.js";
-import Menu from "../components/Menu.js";
-import { Config } from "../config.js";
-import NProgress from "nprogress";
-
-import 'nprogress/nprogress.css';
+import PageWrapper from '../components/PageWrapper.js';
+import Menu from '../components/Menu.js';
+import { Config } from '../config.js';
+import NProgress from 'nprogress';
 
 const headerImageStyle = {
-    marginTop: 50,
-    marginBottom: 50
+	marginTop: 50,
+	marginBottom: 50
 };
 
-Router.onRouteChangeStart = () => NProgress.start();
-Router.onRouteChangeComplete = () => NProgress.done();
+Router.onRouteChangeStart = () => {
+	NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => {
+	NProgress.done();
+};
+
+Router.onRouteChangeError = () => {
+	NProgress.done();
+};
 
 class Index extends Component {
-    static async getInitialProps(context) {
-        const pageRes = await fetch(
-            `${Config.apiUrl}/wp-json/postlight/v1/page?slug=home`
-        );
-        const page = await pageRes.json();
-        const postsRes = await fetch(
-            `${Config.apiUrl}/wp-json/wp/v2/posts?_embed`
-        );
-        const posts = await postsRes.json();
-        const pagesRes = await fetch(
-            `${Config.apiUrl}/wp-json/wp/v2/pages?_embed`
-        );
-        const pages = await pagesRes.json();
-        return { page, posts, pages };
-    }
 
-    render() {
-        console.log(this.props);
-        const posts = this.props.posts.map((post, index) => {
-            return (
-                <ul key={index}>
-                    <li>
-                        <Link
-                            as={`/post/${post.slug}`}
-                            href={`/post?slug=${post.slug}&apiRoute=post`}
-                        >
-                            <a>{post.title.rendered}</a>
-                        </Link>
-                    </li>
-                </ul>
-            );
-        });
-        const pages = this.props.pages.map((page, index) => {
-            return (
-                <ul key={index}>
-                    <li>
-                        <Link
-                            as={`/page/${page.slug}`}
-                            href={`/post?slug=${page.slug}&apiRoute=page`}
-                        >
-                            <a>{page.title.rendered}</a>
-                        </Link>
-                    </li>
-                </ul>
-            );
-        });
-        return (
-            <Layout>
-                <Menu menu={this.props.headerMenu} />
-                <img
-                    src="/static/images/wordpress-plus-react-header.png"
-                    width="815"
-                    style={headerImageStyle}
-                />
-                <h1>{this.props.page.title.rendered}</h1>
-                <div
-                    dangerouslySetInnerHTML={{
-                        __html: this.props.page.content.rendered
-                    }}
-                />
-                <h2>Posts</h2>
-                {posts}
-                <h2>Pages</h2>
-                {pages}
-            </Layout>
-        );
-    }
+	static async getInitialProps(context) {
+		const pageRes = await fetch(
+			`${Config.apiUrl}/wp-json/postlight/v1/page?slug=home`
+		);
+		const page = await pageRes.json();
+		const postsRes = await fetch(
+			`${Config.apiUrl}/wp-json/wp/v2/posts?_embed`
+		);
+		const posts = await postsRes.json();
+		const pagesRes = await fetch(
+			`${Config.apiUrl}/wp-json/wp/v2/pages?_embed`
+		);
+		const pages = await pagesRes.json();
+
+
+		return {
+			page,
+			posts,
+			pages
+		};
+	}
+
+	render() {
+		console.log(this.props);
+		const posts = this.props.posts.map((post, index) =>
+			<ul key={ index }>
+				<li>
+					<Link
+						as={ `/${post.slug}` }
+						href={ `/post?slug=${post.slug}&apiRoute=post` }
+					>
+						<a>{ post.title.rendered }</a>
+					</Link>
+				</li>
+			</ul>
+		);
+		const pages = this.props.pages.map((page, index) =>
+			<ul key={ index }>
+				<li>
+					<Link
+						as={ `/${page.slug}` }
+						href={ `/post?slug=${page.slug}&apiRoute=page` }
+					>
+						<a>{ page.title.rendered }</a>
+					</Link>
+				</li>
+			</ul>
+		);
+
+		return (
+			<Layout>
+				<Menu menu={ this.props.headerMenu } />
+				<img
+					src="/static/images/wordpress-plus-react-header.png"
+					width="815"
+					style={ headerImageStyle }
+				/>
+				<h1>{ this.props.page.title.rendered }</h1>
+				<div
+					dangerouslySetInnerHTML={ {
+						__html: this.props.page.content.rendered
+					} }
+				/>
+				<h2>Posts</h2>
+				{ posts }
+				<h2>Pages</h2>
+				{ pages }
+			</Layout>
+		);
+	}
+
 }
 
 export default PageWrapper(Index);
